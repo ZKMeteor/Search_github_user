f6533e3f8351bebd5bad5bc72947b59620dd4b27
Meteor <97183228+ZKMeteor@users.noreply.github.com>
2023-02-11 05:14:36 +0800

    fix

--- vue.config.js
+++ vue.config.js
@@ -2,19 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave:false,
-/*   devServer: {
-    proxy: {
-      '/api': {
-        target: '<url>',
-        ws: true,
-        changeOrigin: true
-      },
-      '/foo': {
-        target: '<other_url>'
-      }
-    }
-  }, */
   publicPath: process.env.NODE_ENV === 'production'
-  ? '/VueCli_2/'
+  ? '/Search_github_user/'
   : '/'
 })
